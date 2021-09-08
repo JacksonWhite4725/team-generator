6557d4004a86be865111ff41e9d5aa8956cb84c1
JacksonWhite4725 <jawh4725@colorado.edu>
2021-09-07 22:22:22 -0600

    Added full inquirer logic.

--- index.js
+++ index.js
@@ -5,6 +5,74 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-const employee = new Employee;
-employee.employeeInfo();
-console.log(role)
+const team = [];
+
+function buildMember() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your team member name?\n',
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What the team member\'s name?\n',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team member\'s identification number?\n',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team member\'s email address?\n',
+        }
+    ])
+    .then((responses) => {
+        teamMember = [{name: responses.name}, {role: responses.role}, {id: responses.id}, {email: responses.email}];
+        if (responses.role === 'Manager') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'office',
+                    message: `What is ${responses.name}'s office number?\n`,
+                },
+            ])
+            .then((responses) => {
+                teamMember.push({office: responses.office});
+                return teamMember;
+            });
+        } else if (responses.role === 'Engineer') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: `What is ${responses.name}'s GitHub username?\n`,
+                },
+            ])
+            .then((responses) => {
+                teamMember.push({github: responses.github});
+                return teamMember;
+            });
+        } else if (responses.role === 'Intern') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: `What is ${responses.name}'s school name?\n`,
+                },
+            ])
+            .then((responses) => {
+                teamMember.push({school: responses.school});
+                return teamMember;
+            });
+        }
+    });
+};
+
+let teamMember = buildMember();
+team.push(teamMember);
+console.log(team);
