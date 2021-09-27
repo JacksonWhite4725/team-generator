55a777e79eabcf9ba73390d35985272a7ba86cae
JacksonWhite4725 <jawh4725@colorado.edu>
2021-09-27 15:12:15 -0600

    Added Employee.test for initiliazation and methods.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,5 +1,52 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
-    // Test initialization
-})
+    it('Should return assigned name', () => {
+        const name = 'Jackson';
+        const newEmployee = new Employee(name);
+
+        expect(newEmployee.name).tobe(name);
+    });
+
+    it('Should return assigned ID', () => {
+        const id = 1;
+        const newEmployee = new Employee("Jackson", id);
+
+        expect(newEmployee.id).tobe(id);
+    });
+
+    it('Should return assigned email', () => {
+        const email = 'jackson@gmail.com';
+        const newEmployee = new Employee("Jackson", 1, email);
+
+        expect(newEmployee.email).tobe(email);
+    });
+
+    it('Should return name with getName() method', () => {
+        const name = 'Jackson';
+        const newEmployee = new Employee(name);
+
+        expect(newEmployee.getName()).tobe(name);
+    });
+
+    it('Should return ID with getId() method', () => {
+        const id = 1;
+        const newEmployee = new Employee('Jackson', id);
+
+        expect(newEmployee.getId()).tobe(id);
+    });
+
+    it('Should return email with getEmail() method', () => {
+        const email = 'jackson@gmail.com';
+        const newEmployee = new Employee('Jackson', 1, email);
+
+        expect(newEmployee.getEmail()).tobe(email);
+    });
+
+    it('Should return a value of "Employee" with getRole() method', () => {
+        const role = 'Employee',
+        const newEmployee = new Employee('Jackson', 1, 'jackson@gmail.com');
+
+        expect(newEmployee.getRole()).tobe(role);
+    });
+});
