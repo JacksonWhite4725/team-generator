fa9cb475df94752a23b31e1df8bb57b424da2ffa
JacksonWhite4725 <jawh4725@colorado.edu>
2021-09-27 15:21:58 -0600

    Added engineer test

---
+++ test/Engineer.test.js
@@ -0,0 +1,24 @@
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+    it('Should return assigned GitHub account', () => {
+        const github = 'jacksonwhite4725';
+        const newEngineer = new Engineer('Jackson', 1, 'jackson@gmail.com', github);
+
+        expect(newEngineer.github).tobe(github);
+    });
+
+    it('Should return GitHub with getGithub() method', () => {
+        const github = 'jacksonwhite4725';
+        const newEngineer = new Engineer('Jackson', 1, 'jackson@gmail.com', github);
+
+        expect(newEngineer.getGithub()).tobe(github);
+    });
+
+    it('Should return a value of "Engineer" with getRole() method', () => {
+        const role = 'Engineer',
+        const newEngineer = new Engineer('Jackson', 1, 'jackson@gmail.com', 'jacksonwhite4725');
+
+        expect(newEmployee.getRole()).tobe(role);
+    });
+});
